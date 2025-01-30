2f5b838ed5a150bda8c7b4f2362715ebfb9ec3af
skippy <66940824+skippybossx@users.noreply.github.com>
2025-01-30 11:36:28 +0100

    Update api-check-devices.sh

--- librenms/api-check-devices.sh
+++ librenms/api-check-devices.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -H "X-Auth-Token: ${librenms_api_key}" "${librenms_domain_name}/api/v0/devices"
+curl -H "X-Auth-Token: ${librenms_api_key}" "${librenms_domain_name}/api/v0/devices" | jq -r '.devices[] | select(.type=="network") | "\(.hostname) (\(.ip)) - \(.hardware) [\(.version)]"'
