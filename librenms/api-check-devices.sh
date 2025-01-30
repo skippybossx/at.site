2be5d43c521c79756eb9253626bd33ef3d390cb9
skippy <66940824+skippybossx@users.noreply.github.com>
2025-01-30 11:31:16 +0100

    Update api-check-devices.sh

--- librenms/api-check-devices.sh
+++ librenms/api-check-devices.sh
@@ -1,2 +1,6 @@
+ping 10.20.30.50 -c 4
+whoami
+env
+curl -H 'X-Auth-Token: $librenms_api_key' $librenms_domain_name/api/v0/devices
 "curl -H 'X-Auth-Token: $librenms_api_key' $librenms_domain_name/api/v0/devices"
-echo "curl -H 'X-Auth-Token: $librenms_api_key' $librenms_domain_name/api/v0/devices"
+echo "command i run: curl -H 'X-Auth-Token: $librenms_api_key' $librenms_domain_name/api/v0/devices"
