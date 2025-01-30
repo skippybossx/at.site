fc45c7bf3a62b51ab80476513fd703385267009b
skippy <66940824+skippybossx@users.noreply.github.com>
2025-01-30 11:09:21 +0100

    Update api-check-devices.sh

--- librenms/api-check-devices.sh
+++ librenms/api-check-devices.sh
@@ -1,1 +1,1 @@
-curl -H 'X-Auth-Token: {{ librenms_api_key }}' https://{{ librenms_domain_name }}/api/v0/devices
+curl -H 'X-Auth-Token: $librenms_api_key' https://$librenms_domain_name/api/v0/devices
